09864d2976ce48b7cb3eead910537f29aadcb325
MasonAkershoek <81424979+MasonAkershoek@users.noreply.github.com>
2021-03-27 23:33:14 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,166 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <fstream>
+
+using namespace std;
+
+string temp;
+
+void PressEnterToContinue()
+  {
+    cout << "\nPress ENTER to continue... " << flush;
+    cin.ignore( numeric_limits <streamsize> ::max(), '\n' );
+  }
+
+struct info
+{
+  public:
+  string iName;
+  int iamount;
+
+  void collect()
+  {
+    cout << "What is the item?" << endl;
+    cout << ">";
+    cin >> iName;
+    cout << "\nHow many do you have?" << endl;
+    cout << ">";
+    cin >> iamount;
+    cout << "\n\nItem " << iName << " has been added" << endl;
+    PressEnterToContinue();
+    getline(cin, temp);
+  }
+};
+
+class Output
+{
+  public:
+  
+  vector< string > items;
+  vector< int > amount;
+  int del;
+
+  void view(int index)
+  {
+    cout << "\n\nthis is what you have" << endl;
+    cout << "------------------------------" << endl;
+    cout << "INDEX      AMOUNT     ITEMS" << endl;
+    for(int i = 0; i < index; ++i)
+    {
+      cout << i+1 << "            " << amount[i] << "          " << items[i] << endl;
+    }
+    PressEnterToContinue();
+    getline(cin, temp);
+  }
+
+  int goaway(int index)
+  {
+    cout << "\n\nthis is what you have" << endl;
+    cout << "What would you like to deleate?" << endl;
+    cout << "------------------------------" << endl;
+    cout << "INDEX      AMOUNT     ITEMS" << endl;
+    for(int i = 0; i < index; ++i)
+    {
+      cout << i+1 << "            " << amount[i] << "          " << items[i] << endl;
+    }
+    cout << "\n\n>";
+    cin >> del;
+    cout << "\n\nItem " << items[del-1] << " has been deleted" << endl;
+    return del;
+  }
+
+  void save(int index)
+  {
+    ofstream myFile;
+    myFile.open("item.txt");
+    for(int i = 0; i < index; ++i)
+    {
+      myFile << items[i] << endl;
+    }
+    myFile.close();
+
+    myFile.open("amount.txt");
+    for(int i = 0; i < index; ++i)
+    {
+      myFile << amount[i] << endl;
+    }
+    myFile.close();
+  }
+};
+
+int main() {
+  Output myOutput;
+  info myInfo;
+  int quit = 0;
+  int index = 0;
+  int del;
+
+  do
+  {
+    int choice;
+    cout << "\n\nWelcome to the storinator" << endl;
+    cout << "-----------------------------------" << endl;
+    cout << "choose an option\n" << endl;
+    cout << "1. Chech your stored things." << endl;
+    cout << "2. Add items" << endl;
+    cout << "3. Deleate items" << endl;
+    cout << "4. Save" << endl;
+    cout << "5. Load" << endl;
+    cout << "6. Quit" << endl;
+    cout << endl << ">";
+    cin >> choice;
+
+    if(choice == 1)
+    {
+      myOutput.view(index);
+    }
+    else if(choice == 2)
+    {
+      myInfo.collect();
+      myOutput.items.push_back(myInfo.iName);
+      myOutput.amount.push_back(myInfo.iamount);
+      ++index;
+    }
+    else if(choice == 3)
+    {
+      del = myOutput.goaway(index);
+      del = del - 1;
+      myOutput.items.erase(myOutput.items.begin() + del);
+      myOutput.amount.erase(myOutput.amount.begin() + del);
+      --index;
+    }
+    else if(choice == 4)
+    {
+      myOutput.save(index);
+    }
+    else if(choice == 5)
+    {
+      string line;
+      int lins;
+      ifstream myFile;
+      myFile.open("item.txt");
+      while(getline (myFile,line))
+      {
+        myOutput.items.push_back(line);
+      }
+      myFile.close();
+
+      myFile.open("amount.txt");
+      int as;
+      while (myFile >> as)
+      {
+        myOutput.amount.push_back(as);
+        ++index;
+      }
+      myFile.close();
+          }
+    else if(choice == 6)
+    {
+      ++quit;
+      cout << "you have quit" << endl;
+    }
+  }while(quit == 0);
+}
